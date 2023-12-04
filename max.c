cdda586c2335c171ac74c5fcef5f820878f4c05f
nehasri4 <150993194+nehasri4@users.noreply.github.com>
2023-12-05 00:49:22 +0530

    max.c

---
+++ max.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+  int a,b,c;
+scanf("%d%d%d",&a,&b,&c);
+printf(" enter the three numbers");
+if (a>b)
+  printf("a is max);
+    else if (b>c)
+    printf("b is max");
+     else
+       printf("c is max");
+}
