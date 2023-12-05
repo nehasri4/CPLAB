69fca8fa13787f9f3648ddeb6cc8dabbacfc018c
nehasri4 <150993194+nehasri4@users.noreply.github.com>
2023-12-05 12:24:02 +0530

    avg.c

---
+++ avg.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  float a,b,c,sum,avg;
+printf("enter three numbres");
+scanf("%f%f%f",&a,&b,&c);
+sum=a+b+c;
+printf("%f",sum);
+avg=sum/3;
+printf("%f",avg);
+}
+  
