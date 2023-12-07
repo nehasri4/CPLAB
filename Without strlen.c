58b90694835ac653f5e494aef5cd54824c20c23b
nehasri4 <150993194+nehasri4@users.noreply.github.com>
2023-12-07 11:52:45 +0530

    Without strlen.c

---
+++ Without strlen.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+  char str[20];
+  int i,l=0;
+printf ("enter a string:\n");
+scanf("%s",str);
+for (i=0;str[i]!='\0';i++)
+{
+ l++
+   }
+printf ("length of the string:%d",l);
+}
