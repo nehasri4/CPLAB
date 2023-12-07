5f7ce4de87b88ad18af49d122e10a455ef367dd8
nehasri4 <150993194+nehasri4@users.noreply.github.com>
2023-12-07 10:09:49 +0530

    Without string.h

---
+++ Without string.h
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  char s1[100],s2[100];
+int i;
+printf ("enter string s1:");
+scanf ("%s",s1);
+for (i= 0;s1[i]!='\0';++i)
+{
+s2[i]=s1[i];
+}
+s2[i]='\0';
+printf ("string s2:%s",s2);
+}
+
+
